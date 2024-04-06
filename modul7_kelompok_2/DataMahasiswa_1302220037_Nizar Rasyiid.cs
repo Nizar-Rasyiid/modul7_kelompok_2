21b94af7304d4bee19ea4ddd98e4929bd9ce0b87
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2024-04-06 08:05:42 +0700

    second commit

---
+++ modul7_kelompok_2/DataMahasiswa_1302220037_Nizar Rasyiid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_1302220037_Nizar_Rasyiid_AFH
+{
+    internal class DataMahasiswa_1302220037_Nizar_Rasyiid
+    {
+    }
+}
